5132980eca1047580e04775500ab5a7d4f8f6549
Shrawan2002 <shrawankumar1754@gmail.com>
2024-05-23 15:21:52 +0530

    initial files

--- src/CommentsForm.jsx
+++ src/CommentsForm.jsx
@@ -7,18 +7,28 @@ export default function CommentsForm (){
         rating:5
 
     })
+    let handleInputChange = (event)=>{
+        setFormData( (currData)=>{
+            return {...currData,[event.target.name]:event.target.value}
+        })
+    }
+
+    let handleSubmit = (event)=>{
+        event.preventDefault();
+        console.log(formData);
+    }
     return(
         <div>
             <h4>Give a Comments!</h4>
-            <form>
-                <input placeholder="username" type="text" value={formData.username} />
+            <form onSubmit={handleSubmit}>
+                <input placeholder="username" type="text" value={formData.username} onChange={handleInputChange} />
                 <br></br>   <br></br>
-                <textarea placeholder="add few remark" value={formData.remarks}>
+                <textarea placeholder="add few remark" value={formData.remarks} onChange={handleInputChange}>
                     Remark
                 </textarea>
                 <br></br>   <br></br>
                 <input placeholder="rating" type="number" min={1} 
-                max={5}  value={formData.rating} />
+                max={5}  value={formData.rating} onChange={handleInputChange} />
                   <br></br>   <br></br>
                   <button>Add Comment</button>
             </form>
